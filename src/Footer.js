8e236a8c38e23dffb98966f76c8c031f8666fc51
Your GitHub Username <ayushcoding100>
2023-12-19 23:49:04 +0530

    Save changes before branch switch

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
